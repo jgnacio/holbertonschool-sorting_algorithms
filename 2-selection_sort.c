9546726b3caf0f67944e16e12ab62e77a7811388
jgnacio <jgnaciogomez@gmail.com>
2023-01-03 21:28:31 -0300

    add files for selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+  size_t index, out_index;
+  int entry_index, temp_num = 0;
+  
+  if (!array)
+    return;
+
+  for (index = 0; array && index < size; index++)
+  {
+    entry_index = index;
+    for (out_index = index + 1; out_index < size; out_index++)
+    {
+      if (array[out_index] < array[entry_index])
+      {
+        temp_num = array[out_index];
+        array[out_index] = array[entry_index];
+        array[entry_index] = temp_num;
+        print_array(array, size);
+      }
+    }
+  }
+}
