bdec0c0bcef5e20b1a39dd47dbcf112bd5d81695
jgnacio <jgnaciogomez@gmail.com>
2023-01-04 11:50:16 -0300

    fix betty documentation snd coding style

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,29 +1,32 @@
 #include "sort.h"
 
+/**
+ * selection_sort - function that sort an array
+ * @array: array to sort
+ * @size: size of the array
+ */
 void selection_sort(int *array, size_t size)
 {
-  size_t index, out_index;
-  int entry_index, temp_num = 0;
-  
-  if (!array)
-    return;
+	size_t index, out_index;
+	int entry_index, temp_num = 0;
 
-  for (index = 0; array && index < size - 1; index++)
-  {
-    entry_index = index;
-    for (out_index = index + 1; out_index < size; out_index++)
-    {
-      if (array[out_index] < array[entry_index])
-      {
-        entry_index = out_index;
-      }
-    }
-    if (array[index] > array[entry_index])
-    {
-      temp_num = array[index];
-      array[index] = array[entry_index];
-      array[entry_index] = temp_num;
-      print_array(array, size);
-    }
-  }
+	if (!array)
+		return;
+
+	for (index = 0; array && index < size - 1; index++)
+	{
+		entry_index = index;
+		for (out_index = index + 1; out_index < size; out_index++)
+		{
+			if (array[out_index] < array[entry_index])
+				entry_index = out_index;
+		}
+		if (array[index] > array[entry_index])
+		{
+			temp_num = array[index];
+			array[index] = array[entry_index];
+			array[entry_index] = temp_num;
+			print_array(array, size);
+		}
+	}
 }
