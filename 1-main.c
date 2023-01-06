436af6086ac957c80f8122a70b66d834f7dd1c57
jgnacio <jgnaciogomez@gmail.com>
2023-01-06 13:03:47 -0300

    add 1-main.c and update sort.h

---
+++ 1-main.c
@@ -0,0 +1,17 @@
+#include "sort.h"
+int main(void)
+{
+    listint_t *list;
+    int array[] = {19, 48, 71, 99, 1, 13, 52, 96, 73, 86, 7};
+    size_t n = sizeof(array) / sizeof(array[0]);
+
+    list = create_listint(array, n);
+    if (!list)
+        return (1);
+    print_list(list);
+    printf("\n");
+    insertion_sort_list(&list);
+    printf("\n");
+    print_list(list);
+    return (0);
+}
--- sort.h
+++ sort.h
@@ -23,4 +23,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+listint_t *create_listint(const int *array, size_t size);
 #endif
