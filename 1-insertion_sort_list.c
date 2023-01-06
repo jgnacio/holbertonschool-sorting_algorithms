bd1fae52de950e1d42a749636e62be9985f82306
jgnacio <jgnaciogomez@gmail.com>
2023-01-06 13:53:32 -0300

    jgnacio

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,93 +1,32 @@
 #include "sort.h"
 
-void swap(listint_t **node);
-
-
+/**
+ * insertion_sort_list - function that sort a list
+ * @list: the list to sort
+ */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *prev = NULL,
-						*head = *list;
+	listint_t *node = NULL;
 
-	if (!list)
+	if (!list || !(*list))
 		return;
 
-	(void)prev;
-	/*
-	while (head->next)
+	for (node = *list; node; node = node->next)
 	{
-		print_list(*list);
-
-		while (head->prev)
+		while (node->prev && node->prev->n > node->n)
 		{
-			if (head->n < prev->n)
-				swap(&head);
-			prev = head->prev;
-			head = head->prev;
+			node->prev->next = node->next;
+			if (node->next)
+				node->next->prev = node->prev;
+			node->next = node->prev;
+			node->prev = node->prev->prev;
+			node->next->prev = node;
+			if (node->prev)
+				node->prev->next = node;
+			else
+				*list = node;
+			print_list(*list);
 		}
-		head = head->next;
 	}
-	*/
-
-	swap(&(head->next->next));
-	print_list(head);
 }
-
-
-
-
-
-
-
-
-listint_t *create_listint(const int *array, size_t size)
-{
-	listint_t *list;
-	listint_t *node;
-	int *tmp;
-
-	list = NULL;
-	while (size--)
-	{
-		node = malloc(sizeof(*node));
-		if (!node)
-			return (NULL);
-		tmp = (int *)&node->n;
-		*tmp = array[size];
-		node->next = list;
-		node->prev = NULL;
-		list = node;
-		if (list->next)
-			list->next->prev = list;
-	}
-	return (list);
-}
-
-
-void swap(listint_t **node)
-{
-
-	listint_t *temp = NULL,
-						*prev = NULL,
-						*next = NULL,
-						*head = *node;
-			
-	next = head->next;
-	prev = head->prev;
-	temp = next->next;
-
-	if(prev)
-		prev->next = next;
-
-	next->prev = prev;
-	head->prev = next;
-	next->next = head;
-	head->next = temp;
-
-
-
-	if (temp)
-				temp->prev = head;
-}
-
-
 
